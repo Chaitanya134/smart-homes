f7dc01bc6555966a757fd9cb31e310a92c244f5f
Chaitanya134 <chaitanyagiri2@gmail.com>
2023-02-11 19:17:40 +0530

    Add Drawer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
+import { useEffect } from "react"
 import './App.css'
 import LoadingScreen from './components/LoadingScreen'
 import io from "socket.io-client"
+import MiniDrawer from "./components/Drawer";
 
 const socket = io(import.meta.env.VITE_API_URL);
 socket.on("connect", () => {
@@ -8,9 +10,17 @@ socket.on("connect", () => {
 })
 
 function App() {
+  useEffect(() => {
+    console.log("hi");
+    socket.emit("dashboard");
+
+    socket.on("hello", message => console.log({message}));
+  }, [])
+
   return (
     <div className="App">
-      <LoadingScreen />
+      {/* <LoadingScreen /> */}
+      <MiniDrawer />
     </div>
   )
 }
--- server/socket.js
+++ server/socket.js
@@ -1,4 +1,9 @@
 const { io } = require("./server");
 io.on("connection", socket => {
     console.log(socket.id);
-})
+
+    socket.on("dashboard", () => {
+        console.log(socket.id, 'dashhhh');
+        socket.emit("hello", "hello world");
+    });
+});
