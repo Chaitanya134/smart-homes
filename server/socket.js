ef5e5d8942f17efb127d9c178da2d92c9ca59cc4
Chaitanya134 <chaitanyagiri2@gmail.com>
2023-02-12 07:34:00 +0530

    Add Data

--- server/socket.js
+++ server/socket.js
@@ -9,26 +9,11 @@ io.on("connection", socket => {
         const chartData = {};
         socket.emit("start-loading");
         const files = fs.readdirSync("public/data");
-        console.log(files);
 
         const data = await csv({ output: "csv" }).fromFile("public/data/" + fileName.toLowerCase() + ".csv");
         const newData = data.map(row => Number(row[0]));
-        console.log(newData);
-        // console.log(typeof newData);
         chartData[fileName.toLowerCase() + ".csv"] = newData;
 
-
-        // files.forEach(async (file) => {
-        //     console.log(file, fileName.toLowerCase());
-        //     if (!file.includes(fileName.toLowerCase())) return;
-
-        //     const data = await csv({ output: "csv" }).fromFile("public/data/" + file);
-        //     const newData = data.map(row => Number(row[0]));
-        //     console.log(newData);
-        //     // console.log(typeof newData);
-        //     chartData[file] = newData;
-        // });
-        console.log(chartData);
         socket.emit("stop-loading");
         let indices = { [fileName.toLowerCase() + ".csv"]: 0 };
         setInterval(() => {
